a43fe9c41654889eb57fe806cd144a1c13c1ded9
tearexJS <olinovak96@gmail.com>
2021-10-29 15:32:05 +0200

    populated flight

---
+++ sql/V8__insert_flight.sql
@@ -0,0 +1,5 @@
+INSERT INTO aircraft(type, manufacturer, capacity) VALUES ('A380', 'Airbus', 525);
+INSERT INTO aircraft(type, manufacturer, capacity) VALUES ('A350', 'Airbus', 300);
+INSERT INTO aircraft(type, manufacturer, capacity) VALUES ('A320', 'Airbus', 120);
+INSERT INTO aircraft(type, manufacturer, capacity) VALUES ('747', 'Boeing', 416);
+INSERT INTO aircraft(type, manufacturer, capacity) VALUES ('737', 'Boeing', 200);
